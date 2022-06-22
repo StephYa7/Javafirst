9a708c53b61e8a177f1c77dc34051590aec92051
Steph <1000p@mail.ru>
2022-06-22 22:38:58 +0700

    +dz

---
+++ 001/Test.java
@@ -0,0 +1,50 @@
+public class Test {
+    public static void main(String[] args) {
+        double a = 2;
+        int b = -4;
+
+        System.out.println(powRecursion(a, b));
+        System.out.println(powIter(a, b));
+    }
+
+    public static double powRecursion(double a, int b) {
+
+        if (b == 0 && a == 0)
+            return 0.000;
+
+        if (b == 0)
+            return 1;
+
+        if (b < 0)
+            return powRecursion(1 / a, -b);
+
+        double res = powRecursion(a, b / 2);
+
+        return b % 2 == 0 ? res * res : res * res * a;
+    }
+    public static double powIter(double a, int b) {
+        double res = 1;
+        if (a == 0 && b == 0)
+            return 0;
+        if (b == 0)
+            return 1;
+        if (b < 0) {
+            b = -b;
+            a = (1 / a);
+        }
+
+        while (b > 0) {
+            if (b % 2 == 1) {
+                res = res * a;
+            }
+            a = a * a;
+            b = b / 2;
+
+            // System.out.println(res);
+        }
+        return res;
+    }
+}
+
+
+
---
+++ 001/program.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Haha: ");
+        String name = iScanner.nextLine();
+        System.out.printf("Привет, %s!",name);
+        iScanner.close();        
+        
+    }
+            
+}
+/**
+ Жуть
+ */
+
+// public class program {
+
+    
+//     public static void main(String[] args) {
+//         int s = 4242;
+//         s = s ++ -  s;
+
+//         // System.out.println("Hahfewfa");
+//         System.out.println(s);
+
+        
+//     }
+// }
+
+/**
+ * Innerprogram
+ */
+
