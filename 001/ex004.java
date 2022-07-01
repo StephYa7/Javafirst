c88e6ecca509b1df86ede35440e06473a918882c
Steph <1000p@mail.ru>
2022-07-01 11:52:32 +0700

    +

--- 001/ex004.java
+++ 001/ex004.java
@@ -1,19 +1,16 @@
 public class ex004 {
     public static void main(String[] args) {
-        int count = 4;
-        moveRing('A', 'B', 'C', count);
+        int count = 3;
+        moveRing("Start", "Finish", "Buff", count);
     }
 
-    public static void moveRing(char a, char b, char c, int count) {
+    public static void moveRing(String a, String b, String c, int count) {
         if (count == 1) {
             System.out.println(String.format("from %s to %s", a, b));
-        }
-        else{
+        } else {
             moveRing(a, c, b, count - 1);
             System.out.println(String.format("from %s to %s", a, b));
             moveRing(c, b, a, count - 1);
         }
-
     }
-
 }
