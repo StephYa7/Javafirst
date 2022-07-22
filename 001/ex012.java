aaa1ac81a9bb4963882b27c6c220e4829f14992d
Steph <1000p@mail.ru>
2022-07-22 10:27:17 +0700

    perenes

--- 001/ex012.java
+++ 001/ex012.java
@@ -10,29 +10,33 @@ public class ex012 {
     public static void main(String[] args) {
         String Start = "{a+]}{(d*3)}";
         Check(Start);
-
     }
     public static void Check(String start) {
-        String open ="({[";
-        String close = ")}]";
-        StringBuilder buff = new StringBuilder();   
+    int countfirst = 0;
+    int countsecond = 0;
+    int counthird = 0;
+    int countfourth = 0;
+
         for (int i = 0; i < start.length(); i++){
-            // System.out.println(start.charAt(i));
+            if (start.charAt(i)== '(') countfirst++;
+            if (start.charAt(i)== ')') countfirst--;
+            if (countfirst<0){ System.out.println("false");
+                System.exit(1); ;}
+            if (start.charAt(i)== '{') countsecond++;
+            if (start.charAt(i)== '}') countsecond--;
+            if (countsecond<0){ System.out.println("false");
+                System.exit(1) ;}
+            if (start.charAt(i)== '[') counthird++;
+            if (start.charAt(i)== ']') counthird--;
+            if (counthird<0){ System.out.println("false");
+                System.exit(1) ;}
+            if (start.charAt(i)== '<') countfourth++;
+            if (start.charAt(i)== '>') countfourth--;
+            if (counthird<0){ System.out.println("false");
+                System.exit(1) ;}
 
-            for (int j = 0;j < open.length(); j++){
-                if (start.charAt(i) == open.charAt(j)){
-                    buff.append(start.charAt(i));
-                }
-            }
-            for (int j = 0;j < close.length(); j++){
-                if (start.charAt(i) == close.charAt(j)){
-                    for (int q = 0;j < buff.length(); q++){
-                    
-                }
-
-            }
-        }}
-    System.out.println(buff);
+        }
+    if (countfirst!=0 || countsecond!=0 || counthird!=0 || countfourth !=0 ) System.out.println("false");
+    else System.out.println("true");
     }
-    
 }
