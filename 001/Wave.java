80aa0f44208078fa26f97b7799d15974647ea5c8
Steph <1000p@mail.ru>
2022-08-05 01:50:24 +0700

    Wave from test

--- 001/Wave.java
+++ 001/Wave.java
@@ -1,26 +1,162 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
 
+public class Wave {
+    public static void main(String[] args) {
 
-// public class Wave {
-//     void run(Scanner in, PrintWriter out){
-//         class Virus {
-//             final int x, y;
-//             Virus(int x, int y) { this.x = x; this.y = y; }
-//             int to(int tox, int toy) { return Math.abs(tox - x) + Math.abs(toy - y); }
-//         }
-//         final int width = in.nextInt(), height = in.nextInt();
-//         final Virus[] viruses;
-//         Arrays.setAll(viruses = new Virus[in.nextInt()], n -> new Virus(in.nextInt(), in.nextInt()));
- 
-//         int result = Integer.MIN_VALUE;
- 
-//         for (int x = 1; x <= width; x++)
-//             for (int y = 1; y <= height; y++) {
-//                 int min = Integer.MAX_VALUE;
-//                 for (Virus v : viruses) min = Math.min(min, v.to(x, y));
-//                 result = Math.max(result, min);
-//             }
-//         out.println(result);
-//         out.flush();
-//     }
-    
-// }
+        String xStart = "1";
+        String yStart = "1";
+        int xSize = 12;
+        int ySize = 12;
+        int xFinish = 9;
+        int yFinish = 9;
+        String[][] board = new String[xSize][ySize];
+
+        paintBoard(board, xFinish, yFinish);
+
+        setBoard(board, xStart, yStart);
+
+        showBoard(board);
+
+        System.out.println(wayToStart(board, xFinish, yFinish));
+
+    }
+
+    public static ArrayList<String> wayToStart(String[][] board, int xFinish, int yFinish) {
+
+        ArrayList<String> result = new ArrayList<String>();
+
+        if (isNumber(board[xFinish][yFinish]) == false) {
+            result.add("Нет решений");
+            return result;
+        }
+
+        while (Integer.parseInt(board[xFinish][yFinish]) > 1) {
+
+            if ((isNumber(board[xFinish - 1][yFinish]) == true) && (Integer.parseInt(board[xFinish - 1][yFinish])
+                    + 1) == (Integer.parseInt(board[xFinish][yFinish]))) {
+                xFinish -= 1;
+            } else if ((isNumber(board[xFinish + 1][yFinish]) == true) && (Integer.parseInt(board[xFinish + 1][yFinish])
+                    + 1) == (Integer.parseInt(board[xFinish][yFinish]))) {
+                xFinish += 1;
+            } else if ((isNumber(board[xFinish][yFinish - 1]) == true) && (Integer.parseInt(board[xFinish][yFinish - 1])
+                    + 1) == (Integer.parseInt(board[xFinish][yFinish]))) {
+                yFinish -= 1;
+            } else if ((isNumber(board[xFinish][yFinish + 1]) == true) && (Integer.parseInt(board[xFinish][yFinish + 1])
+                    + 1) == (Integer.parseInt(board[xFinish][yFinish]))) {
+                xFinish += 1;
+            }
+            result.add("{ x = " + Integer.toString(xFinish));
+            result.add(" y = " + Integer.toString(yFinish) + "}");
+        }
+        return result;
+    }
+
+    private static boolean isNumber(String s) throws NumberFormatException {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static void setBoard(String[][] board, String xStart, String yStart) {
+        int count = 1;
+        Stack<String> bufSetNext = new Stack<>();
+        Stack<String> bufSet = new Stack<>();
+
+        bufSetNext.push(xStart);
+        bufSetNext.push(yStart);
+
+        while (!bufSetNext.isEmpty() || !bufSetNext.isEmpty()) {
+
+            while (!bufSetNext.isEmpty()) {
+                int y = Integer.parseInt(bufSetNext.pop());
+                int x = Integer.parseInt(bufSetNext.pop());
+                if (board[x + 1][y] == "O") {
+                    board[x + 1][y] = Integer.toString(count);
+                    bufSet.push(Integer.toString(x + 1));
+                    bufSet.push(Integer.toString(y));
+                }
+                if (board[x][y + 1] == "O") {
+                    board[x][y + 1] = Integer.toString(count);
+                    bufSet.push(Integer.toString(x));
+                    bufSet.push(Integer.toString(y + 1));
+                }
+                if (board[x - 1][y] == "O") {
+                    board[x - 1][y] = Integer.toString(count);
+                    bufSet.push(Integer.toString(x - 1));
+                    bufSet.push(Integer.toString(y));
+                }
+                if (board[x][y - 1] == "O") {
+                    board[x][y - 1] = Integer.toString(count);
+                    bufSet.push(Integer.toString(x));
+                    bufSet.push(Integer.toString(y - 1));
+                }
+            }
+            count += 1;
+            while (!bufSet.isEmpty()) {
+                int y = Integer.parseInt(bufSet.pop());
+                int x = Integer.parseInt(bufSet.pop());
+                if (board[x + 1][y] == "O") {
+                    board[x + 1][y] = Integer.toString(count);
+                    bufSetNext.push(Integer.toString(x + 1));
+                    bufSetNext.push(Integer.toString(y));
+                }
+                if (board[x][y + 1] == "O") {
+                    board[x][y + 1] = Integer.toString(count);
+                    bufSetNext.push(Integer.toString(x));
+                    bufSetNext.push(Integer.toString(y + 1));
+                }
+                if (board[x - 1][y] == "O") {
+                    board[x - 1][y] = Integer.toString(count);
+                    bufSetNext.push(Integer.toString(x - 1));
+                    bufSetNext.push(Integer.toString(y));
+                }
+                if (board[x][y - 1] == "O") {
+                    board[x][y - 1] = Integer.toString(count);
+                    bufSetNext.push(Integer.toString(x));
+                    bufSetNext.push(Integer.toString(y - 1));
+                }
+            }
+            count += 1;
+
+        }
+        board[Integer.parseInt(xStart)][Integer.parseInt(yStart)] = "0";
+    }
+
+    public static void paintBoard(String[][] board, int xFinish, int yFinish) {
+        Random random = new Random();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                int pointRandom = random.nextInt(11);
+                if (pointRandom > 9)
+                    board[i][j] = "X";
+                else
+                    board[i][j] = "O";
+            }
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (i == 0 || j == 0 || i == board.length - 1 || j == board[0].length - 1) {
+                    board[i][j] = "X";
+                }
+
+            }
+        }
+
+        board[xFinish][yFinish] = "O";
+    }
+
+    public static void showBoard(String[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.printf(board[i][j]);
+                System.out.printf(" ");
+            }
+            System.out.println("");
+        }
+    }
+}
